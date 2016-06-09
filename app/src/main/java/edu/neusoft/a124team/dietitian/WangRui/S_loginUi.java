2042629591c2a599536eb3f3f9deabf4338a1fd7
Aria <842915974@qq.com>
2016-06-09 17:38:11 +0800

    部分登录界面

---
+++ app/src/main/java/edu/neusoft/a124team/dietitian/WangRui/S_loginUi.java
@@ -0,0 +1,16 @@
+package edu.neusoft.a124team.dietitian.WangRui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import edu.neusoft.a124team.dietitian.R;
+
+public class S_loginUi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_s_login);
+        setTitle("登录");
+    }
+}
--- app/src/main/java/edu/neusoft/a124team/dietitian/fragment5.java
+++ app/src/main/java/edu/neusoft/a124team/dietitian/fragment5.java
@@ -1,19 +1,31 @@
 package edu.neusoft.a124team.dietitian;
 
-        import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
         import android.support.annotation.Nullable;
         import android.support.v4.app.Fragment;
         import android.view.LayoutInflater;
         import android.view.View;
         import android.view.ViewGroup;
+import android.widget.Button;
+
+import edu.neusoft.a124team.dietitian.WangRui.S_loginUi;
 
 
 public class fragment5 extends Fragment {
-
+private Button btn;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.layout5, container, false);
+        btn=(Button)view.findViewById(R.id.s_circle_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getActivity(), S_loginUi.class);
+                startActivity(i);
+            }
+        });
 
         return view;
     }
