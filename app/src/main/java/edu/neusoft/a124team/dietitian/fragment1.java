3289afe92e762977886a6b8b3c26619f3b1e3dd6
wh415471768 <415471768@qq.com>
2016-06-09 10:49:52 +0800

    恢复主干分支

--- app/src/main/java/edu/neusoft/a124team/dietitian/fragment1.java
+++ app/src/main/java/edu/neusoft/a124team/dietitian/fragment1.java
@@ -14,7 +14,9 @@ public class fragment1 extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         View view = inflater.inflate(R.layout.layout1, container, false);
+
         return view;
 
     }
