d9b47f3af7f73bedb56c709c333c6bbf8f7ef844
Aria <842915974@qq.com>
2016-06-09 10:35:24 +0800

    bug分支

--- app/src/main/java/edu/neusoft/a124team/dietitian/fragment1.java
+++ app/src/main/java/edu/neusoft/a124team/dietitian/fragment1.java
@@ -11,15 +11,12 @@ import android.view.ViewGroup;
 
 
 public class fragment1 extends Fragment {
-
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         View view = inflater.inflate(R.layout.layout1, container, false);
-
         return view;
+
     }
 
 }
